22d669619cac3214cd3a0cde9d07a9c2089bafb9
Axel <104387292+Wislia@users.noreply.github.com>
2026-02-17 16:59:40 +0100

    build 1

---
+++ Assets/Script/ButtonLeftRight.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Script
+{
+    public class ButtonLeftRight : MonoBehaviour//, ISelectHandler
+    {
+        private MenuManager _menuManager;
+        
+        [SerializeField] private Image left;
+        [SerializeField] private Image right;
+        
+        private GameObject _children;
+        private void Start()
+        {
+            _menuManager = MenuManager.Instance;
+            _children = transform.GetChild(0).gameObject;
+            GetComponent<Button>().onClick.AddListener(OnClick);
+        }
+
+
+        /*public void OnSelect(BaseEventData eventData)
+        {
+            _menuManager.DestroyButtons();
+            _menuManager.ChangeGameObjectArmLegState(false);
+            if (!_children.activeSelf) _children.SetActive(true);
+        }*/
+        
+        public void OnClick()
+        {
+            _menuManager.DestroyButtons();
+            _menuManager.ChangeGameObjectArmLegState(false);
+            if (!_children.activeSelf) _children.SetActive(true);
+        }
+        
+        
+    }
+}
